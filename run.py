a7b8bb4475e0f4f55cc0fc524627ae301cee85e3
rickWsl <13618318362@163.com>
2023-03-05 10:40:31 +0800

    脚本完成

--- run.py
+++ run.py
@@ -1,6 +1,28 @@
 # 导入os模块
 import os
+import shutil
+def insertContent():
+    # 打开文件A和文件B
+    pathA = "/mnt/d/jetaime/softdocument/siyuanWokerSpace/software/data/plugins/mytest/main.js"
+    pathB = os.path.join(".", "build", "main.js")
+    with open(pathA, "r+") as fileA, open(pathB, "r+") as fileB:
+      # Read the first 31 lines of fileA and delete the rest
+      linesA = fileA.readlines()[:31]
+      fileA.seek(0)
+      fileA.truncate()
+      fileA.writelines(linesA)
 
+      linesB = fileB.readlines()[1:]
+      # 读取文件B的全部内容，并删除前5个字符
+      contentB = linesB[0][5:]
+      # 在前面添加function myshow()
+      contentB = "function myshow()" + contentB
+      # 删除最后4个字符
+      contentB = contentB[:-4]
+      
+    # 将文件B的内容加到文件A的31行后
+    with open(pathA, "a") as fileA:
+        fileA.write(contentB)
 # 获取当前操作系统的名称
 system = os.name
 
@@ -13,25 +35,10 @@ else: # 其他系统
   print("Unsupported system.")
   exit()
 
-# 在当前文件夹下执行yarn run build命令
-os.system(f"{cmd} -c 'yarn run build'")
-
-# 打开文件A和文件B
-paths = "d:\jetaime\softdocument\siyuanWokerSpace\software\data\plugins\mytest\main.js"
-with open(paths, "r+") as fileA, open("./build/main.js", "r+") as fileB:
-  # 读取文件A的前31行，并删除其余内容
-  linesA = fileA.readlines()[:31]
-  fileA.seek(0)
-  fileA.truncate()
-  fileA.writelines(linesA)
-
-  # 读取文件B的全部内容，并删除前5个字符
-  contentB = fileB.read()[5:]
-  # 在前面添加function myshow()
-  contentB = "function myshow()" + contentB
-  # 删除最后4个字符
-  contentB = contentB[:-4]
-
-# 将文件B的内容加到文件A的31行后
-with open("fileA.js", "a") as fileA:
-  fileA.write(contentB)
+status = os.system(f"{cmd} -c 'yarn run build'")
+# 判断状态码是否为0
+if status == 0:
+  print("打包成功")
+  insertContent()
+else:
+  print("Command failed with status code:", status)
